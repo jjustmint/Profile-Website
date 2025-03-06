a268d3452f516f2ea80557b4f88db476bf98b4a9
jjustmint <123088410+jjustmint@users.noreply.github.com>
2025-03-06 19:19:46 +0700

    Update gallery.jsx

--- profile-website/src/pages/gallery.jsx
+++ profile-website/src/pages/gallery.jsx
@@ -1,8 +1,34 @@
 const Gallery = () => {
     return (
-        <div>
-            <h1></h1>
+      <div className="flex flex-col items-center min-h-screen">
+        <h1 className="font-bold text-5xl mt-20">Gallery</h1>
+        <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 p-10">
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
+          <img
+            className="w-200 rounded-lg shadow-md"
+            src="https://iconext.co.th/wp-content/uploads/2021/07/UXUI-Design_image-510x280-1.png"
+          />
         </div>
+      </div>
     );
 }
 
