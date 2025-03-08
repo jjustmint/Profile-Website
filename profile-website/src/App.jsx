f4e0bc7b7115abe7d513a521792c607c3e29d7f0
APOM-Tawan <119406378+APOM-Tawan@users.noreply.github.com>
2025-03-08 23:25:53 +0700

    Update App.jsx

--- profile-website/src/App.jsx
+++ profile-website/src/App.jsx
@@ -2,12 +2,15 @@ import "./App.css";
 import Home from "./pages/home";
 import Navbar from "./assets/navbar";
 import Gallery from "./pages/Gallery";
+import About_me from "./pages/aboutme";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home/>
+      <About_me/>
+      <Gallery/>
     </div>
   );
 }
